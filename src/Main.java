886ccb35be62f70e1e374609cd924bed361a680a
ibrada <radikibragimov@mail.ru>
2023-11-06 02:17:15 +0300

    hw16 is done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
 public class Main {
+
+    public static void line() {
+        System.out.println("* * * * *");
+    }
+
     public static void main(String[] args) {
+
+        Car car = new Car("car1", 4);
+        Car car2 = new Car("car2", 4);
+
+        Truck truck = new Truck("truck1", 6);
+        Truck truck2 = new Truck("truck2", 8);
+
+        Bicycle bicycle = new Bicycle("bicycle1", 2);
+        Bicycle bicycle2 = new Bicycle("bicycle2", 2);
+
+        ServiceStation station = new ServiceStation();
+        line();
+        station.check(car);
+        line();
+        station.check(car2);
+        line();
+        station.check(bicycle);
+        line();
+        station.check(bicycle2);
+        line();
+        station.check(truck);
+        line();
+        station.check(truck2);
+        line();
 
     }
 }
---
+++ src/Vehicle.java
@@ -0,0 +1,29 @@
+public abstract class Vehicle {
+
+    private String modelName;
+    private int wheelsCount;
+
+    public Vehicle(String modelName, int wheelsCount) {
+        this.modelName = modelName;
+        this.wheelsCount = wheelsCount;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public int getWheelsCount() {
+        return wheelsCount;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public void setWheelsCount(int wheelsCount) {
+        this.wheelsCount = wheelsCount;
+    }
+
+    public void check() {
+    }
+}
